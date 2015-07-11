f6697ab0817384eac8e2a6f57f610640fa028136
Mark <markthebruce@gmail.com>
2015-07-11 17:49:38 +0930

    portalactions.py

---
+++ portalactions.py
@@ -0,0 +1,188 @@
+def door():
+	print "\n"
+	print "You walk up to the door and tap the touch pad. It slides open and "
+	print "you step through into the next room as the door slides shut behind "
+	print "you."
+	print "\n"
+		
+		
+def ceilingwall():
+	print "\n"
+	print "You jump up to the door in the ceiling and tap the touch pad. It "
+	print "slides open and you jump up again, grab the edges, and haul yourself "
+	print "up. As you pass the threshold gravity shifts jarringly and disorientingly "
+	print "beneath you. You tumble along what you thought was the floor, and quickly "
+	print "hit a wall . . . which as you get to your feet you realise is actually "
+	print "the floor now, and the door you came through now slides shut in the wall "
+	print "next to you."
+	print "\n"
+
+		
+def wallceiling():
+	print "\n"
+	print "You tap the touchpad, the door slides open, and you step through into "
+	print "the room. As you cross the threshold gravity shifts alarmingly beneath "
+	print "you and you fall forward, plunging the length of the room, panic spikes "
+	print "and you just manage to bring your hands up before smacking smacking "
+	print "heavily into the door on the far wall. Gingerly getting to your feet "
+	print "you realise this 'wall' is now the floor."
+	print "\n"
+		
+		
+def floorwall():
+	print "\n"
+	print "You crouch down by the door on the floor, and it slides open as you "
+	print "tap the touchpad. Hanging your legs over the edge you lower yourself "
+	print "down and feel gravity twisting towards a wall. You go with it, lowering "
+	print "yourself to the wall, which is now the floor, and the door that was in "
+	print "the floor now closes in the wall you just left."
+	print "\n"
+		
+		
+def wallfloor():
+	print "\n"
+	print "You walk to the door and it slides open after you tap the touchpad. "
+	print "Walking through the threshold gravity shifts confusingly and you fall "
+	print "back towards the wall and stumble back into the room you just tried to "
+	print "leave. Grabbing the sides of the door this time you haul yourself "
+	print "through the opening as you step through, rolling onto the wall on the "
+	print "other side, which as you come to your feet you realise it is now the "
+	print "floor."
+	print "\n"
+		
+		
+def ceilingceiling():
+	print "\n"
+	print "You jump up and tap the touchpad and the door slides open. Jumping up "
+	print "again to grab the side of the door you haul yourself up and through; "
+	print "too late you gasp as gravity shifts dangerously over you and you fall "
+	print "quickly upwards towards the next ceiling, only just managing to roll "
+	print "enough not to hit your head, but still knocking the wind out of you. "
+	print "Standing on this new floor you look up as the door closes in the "
+	print "ceiling you thought was a floor."
+	print "\n"
+		
+		
+def floorfloor():
+	print "\n"
+	print "You crouch down by the door, tap the pad, and watch the door slide open. "
+	print "Sitting on the edge of the doorway you dangle your feet through into the "
+	print "next room and feel gravity push them straight back towards you; for a moment "
+	print "your feet hang weightless in the middle of the doorway. You push them out "
+	print "again and to one side then confusingly roll your body through to rest on "
+	print "what you thought was the ceiling of the room below, but as you come to your "
+	print "feet you reorient to this new floor."
+	print "\n"
+
+	
+def floorceiling():
+	print "\n"
+	print "You crouch down by the door on the floor, and it slides open as you tap the "
+	print "touchpad. Hanging your legs over the edge you lower yourself down gently "
+	print "before finally letting go of the edges of the door and falling the short "
+	print "distance to the floor below as the door closes above you."
+	print "\n"
+
+
+def ceilingfloor():
+	print "\n"
+	print "You jump up and tap the touchpad and the door slides open. Jumping up again "
+	print "to grab the side of the door you haul yourself up and through, coming to stand "
+	print "on the floor above as the door slides shut beneath you."
+	print "\n"
+
+
+def corpse():
+	print "\n"
+	print "The body, a man, is partially decayed with wrinkled ashen skin "
+	print "wrapping the bones and making the clothes look too big for his frame. "
+	print "He lies crumpled against the corner, head resting on a shoulder, "
+	print "mouth agape and white teeth showing past taut atrophied lips. A hand "
+	print "rests on the floor, open, and in the palm you see a tiny cube. You "
+	print "pick the cube up and see that it is actually a piece of paper folded "
+	print "up to look like a cube and unfolding it reveals this shape: "
+	print "\n"
+	print "               ______"
+	print "              |      |"
+	print "              |      |"
+	print " ______ ______|______|______"
+	print "|      |      |      |      |"
+	print "|      |      |      |      |"
+	print "|______|______|______|______|"
+	print "              |      |"
+	print "              |      |"
+	print "              |______|"
+	print "\n"
+	print "You leave the corpse to rest."
+	print "\n" 
+
+
+def sculpture():
+	print "\n"
+	print "The sculpture is of two hyperbolic hemispheres that seem to hang in "
+	print "the air, inverted with their apexes facing each other and touching. "
+	print "On closer inspection you realise that the hyperbolas aren't actually "
+	print "touching at all; they come very very close together but never actually "
+	print "touch."
+	print "\n"
+	
+	
+def pattern():
+	print "\n"
+	print "You inspect the pattern and notice the flames in this pattern form "
+	print "a figure of eight lying on its side with the flames flowing along its "
+	print "curves continuously."
+	print "\n"
+	
+	
+def stain():
+	print "\n"
+	print "Getting close to the blood stain you notice, in among the smudges "
+	print "and fingerprints, someone has drawn the following shape:"
+	print "\n"
+	print "               ______"
+	print "              |      |"
+	print "              |      |"
+	print " ______ ______|______|______"
+	print "|      |      |      |      |"
+	print "|      |      |      |      |"
+	print "|______|______|______|______|"
+	print "              |      |"
+	print "			     |      |"
+	print "			     |______|"
+	print "\n"
+	
+	
+def runes():
+	print "\n"
+	print "Getting up close to the runes, carved with some precision into the "
+	print "emerald, you can read the following formula: Delta = x + y + z + i "
+	print "All the symbols are normal except the 'i', which is italicised."
+	print "\n"
+	
+
+def sun():
+	print "\n"
+	print "Getting close you see that the little light source is suspended in air "
+	print "6 inches from the wall, and between it and the wall, unseen until now, "
+	print "hangs a little hollow cube whose edges are the only solid pieces; the "
+	print "light causes it to cast a faint shadow on the wall that looks like this: "
+	print "\n"
+	print "    _______" 
+	print "   /|     /|"
+	print "  / |    / |"
+	print " /__|___/  |" 
+	print "|   |___|__|"
+	print "|  /    |  /"
+	print "| /     | /" 
+	print "|/______|/"
+	print "\n"
+	print "A classic Necker cube."
+	print "\n"
+	print "Above the shape there appears to be another shadow of an object but "
+	print "there is no object to be seen hanging like the cube is. The shape of "
+	print "this other shadow is more complex and looks like a small Necker cube "
+	print "sitting in the centre of a larger Necker cube and with each pointed "
+	print "vertex of the inner cube joined by a line to the corresponding vertex "
+	print "of the larger cube."
+	print "\n"
